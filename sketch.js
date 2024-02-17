2e421e0406eed6e1ea693b223e369b5f8eaa6b6d
jsmelchorg <160347054+jsmelchorg@users.noreply.github.com>
2024-02-17 14:20:40 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+  createCanvas(320, 240);
+  
+}
+
+function draw() {
+  background(100, 0, 300);
+  circle(160, 120, 190);
+  circle (130, 100, 35);
+  circle (190, 100, 35);
+  triangle(40, 75, 58, 40, 86, 75);
+  //background(220);
+}
